7ca93a7ab1b1d365edcd1d01e8ba3bf974efbb94
Chrstjan <ps5christian@hotmail.com>
2024-09-19 09:23:30 +0200

    Made most of the desktop styling

--- History Site/src/components/Main/About/About.jsx
+++ History Site/src/components/Main/About/About.jsx
@@ -9,7 +9,7 @@ export const About = ({isDarkMode}) => {
                     </header>
                     <p>Ever wondered what happened on this very day throughout history? Or perhaps you’re curious about a specific date in the past? At Time Traveler’s Hub, we’ve got your curiosity covered! Since 1947, we’ve been archiving the world’s most fascinating moments—from ground-shaking historical events to quirky, forgotten tales.</p>
                     <p>Unlike some other “time machines” out there that only rewind websites, we’re all about diving into the real-life drama and intrigue of human history.</p>
-                    <p>Who knows, you might even stumble upon sightings of a man dressed as a spider or other intriguing snippets from the annals of history!</p>
+                    <p>Who knows, you might even stumble upon sightings of a man dressed as a spider fighting crime or other intriguing snippets from the annals of history!</p>
                 </article>
         </>
     )
--- History Site/src/components/Navigation/Navigation.jsx
+++ History Site/src/components/Navigation/Navigation.jsx
@@ -4,15 +4,15 @@ import style from "./Navigation.module.scss";
 export const Navigation = ({isDarkMode}) => {
     return (
         <nav className={`${style.navStyling} ${isDarkMode ? null : style.lightMode}`}>
-            <ul className={style.NavigationStyling}>
+            <ul className={`${style.NavigationStyling} ${isDarkMode ? null : style.lightMode}`}>
                 <li>
-                    <NavLink style={({isActive}) => isActive ? {textDecoration: "underline", color: "#ffe9bf"} : {color: "#c7bd8d"}} to={"/by-date"}>By Date</NavLink>
+                    <NavLink className={({isActive}) => isActive ? style.activeLink : style.navLink} to={"/by-date"}>By Date</NavLink>
                 </li>
                 <li>
-                    <NavLink style={({isActive}) => isActive ? {textDecoration: "underline", color: "#ffe9bf"} : {color: "#c7bd8d"}} to={"/"}>Today</NavLink>
+                    <NavLink className={({isActive}) => isActive ? style.activeLink : style.navLink} to={"/"}>Today</NavLink>
                 </li>
                 <li>
-                    <NavLink style={({isActive}) => isActive ? {textDecoration: "underline", color: "#ffe9bf"} : {color: "#c7bd8d"}}  to={"/since"}>Since</NavLink>
+                    <NavLink className={({isActive}) => isActive ? style.activeLink : style.navLink} to={"/since"}>Since</NavLink>
                 </li>
             </ul>
         </nav>
